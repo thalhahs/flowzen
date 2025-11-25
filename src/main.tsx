2e28d4c6fd9ed7e3ba69a748dfdfe80a24c9cea2
thalhahs <thalhah@gmail.com>
2025-11-25 21:21:44 +0700

    feat: about route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import "./index.css";
-import { App } from "./app";
+import "@/index.css";
+import { App } from "@/app";
+import { About } from "@/routes/about";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   </BrowserRouter>
 );
